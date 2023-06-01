fe040aa56b676832d37640aec63718f0b0958d29
DeShon Dixon <deshondixon@gmail.com>
2023-06-01 13:13:53 -0700

    acp

--- src/main/java/com/revature/daos/RoleDAO.java
+++ src/main/java/com/revature/daos/RoleDAO.java
@@ -56,7 +56,7 @@ public class RoleDAO implements RoleDAOInterface {
 
             PreparedStatement ps = conn.prepareStatement(sql);
 
-
+            ps.setInt(1, salary);
 
         } catch(SQLException e){
             System.out.println("Update Failed");
