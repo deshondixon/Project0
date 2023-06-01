05d26c92724cc85211095f97359c69dd275d7686
DeShon Dixon <deshondixon@gmail.com>
2023-06-01 09:12:58 -0700

    acp

--- src/main/java/com/revature/Launcher.java
+++ src/main/java/com/revature/Launcher.java
@@ -3,13 +3,16 @@ package com.revature;
 import com.revature.utils.ConnectionUtil;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Launcher {
     public static void main(String[] args) {
 
         try(Connection conn = ConnectionUtil.getConnection()) {
-
-
+            System.out.println("Connection Successful");
+        }
+        catch(SQLException e){
+            System.out.println("Connection Failed");
         }
     }
 }
