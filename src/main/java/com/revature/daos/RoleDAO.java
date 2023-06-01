bc8da598e61ba0ab88c9d1130bde80519b963aad
DeShon Dixon <deshondixon@gmail.com>
2023-06-01 11:48:21 -0700

    acp

--- src/main/java/com/revature/daos/RoleDAO.java
+++ src/main/java/com/revature/daos/RoleDAO.java
@@ -1,5 +1,24 @@
 package com.revature.daos;
 
-public class RoleDAO {
+import com.revature.models.Role;
+import com.revature.utils.ConnectionUtil;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class RoleDAO implements RoleDAOInterface {
+
+    @Override
+    public Role getRoleByID(int id) {
+
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            String sql = "SELECT * FROM roles WHERE role_id = ?";
+
+        } catch (SQLException e) {
+            System.out.println("error getting Role");
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
+
