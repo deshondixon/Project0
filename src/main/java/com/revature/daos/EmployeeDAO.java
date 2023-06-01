3afe5fe110017b16c550cae3f10ed0e1d1f6dd75
DeShon Dixon <deshondixon@gmail.com>
2023-06-01 13:48:01 -0700

    acp

--- src/main/java/com/revature/daos/EmployeeDAO.java
+++ src/main/java/com/revature/daos/EmployeeDAO.java
@@ -1,4 +1,17 @@
 package com.revature.daos;
 
+import com.revature.models.Employee;
+
+import java.util.ArrayList;
+
 public class EmployeeDAO implements EmployeeDAOInterface {
+    @Override
+    public ArrayList<Employee> getAllEmployees() {
+        return null;
+    }
+
+    @Override
+    public Employee insertEmployee(Employee emp) {
+        return null;
+    }
 }
