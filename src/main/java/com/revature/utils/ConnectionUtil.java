eb18f92837667e1d4a1da6e8f66fe267d665ba6e
DeShon Dixon <deshondixon@gmail.com>
2023-06-01 08:55:14 -0700

    acp

--- src/main/java/com/revature/utils/ConnectionUtil.java
+++ src/main/java/com/revature/utils/ConnectionUtil.java
@@ -31,9 +31,9 @@ public class ConnectionUtil {
         //Hardcoded for now - It's posible hide them in the Environment Variables, feel free to look into it
 
         //I'm going to put the credentials in Strings, and use those strings in a method that gets connections
-        String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=<schema_name>";
+        String url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=project0";
         String username = "postgres";
-        String password = "<your password here>";
+        String password = "password";
 
         //This return statement is what returns out actual database Connection object
         //Note how this getConnection() method has a return type of Connection
