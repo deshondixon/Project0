d5bfaef1a448188c5f5b50119f8438d09f81a492
DeShon Dixon <deshondixon@gmail.com>
2023-06-01 09:05:00 -0700

    acp

--- src/main/java/com/revature/Launcher.java
+++ src/main/java/com/revature/Launcher.java
@@ -1,7 +1,15 @@
 package com.revature;
 
+import com.revature.utils.ConnectionUtil;
+
+import java.sql.Connection;
+
 public class Launcher {
     public static void main(String[] args) {
 
+        try(Connection conn = ConnectionUtil.getConnection()) {
+
+
+        }
     }
 }
