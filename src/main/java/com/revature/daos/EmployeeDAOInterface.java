a24fe587151b342d23696c6ef7c76fa9ae6b7ce5
DeShon Dixon <deshondixon@gmail.com>
2023-06-01 13:37:54 -0700

    acp

--- src/main/java/com/revature/daos/EmployeeDAOInterface.java
+++ src/main/java/com/revature/daos/EmployeeDAOInterface.java
@@ -1,5 +1,14 @@
 package com.revature.daos;
 
+import com.revature.models.Employee;
+import com.revature.models.Role;
+
+//Remember, the main reason I like Interfaces is for organization and clarity
+//DAO methods are long. Imagine we have 20 DAO methods. An Interface is a quick way to see what methods we have.
 public interface EmployeeDAOInterface {
+
+    Employee getRoleByID(int id);
+
+    boolean updateRoleSalary(int salary, String title);
 
 }
