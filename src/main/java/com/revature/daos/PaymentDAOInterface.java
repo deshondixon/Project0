625719060b70a8d5ee6437dbc809a9b1e7905f1c
DeShon Dixon <deshondixon@gmail.com>
2023-06-04 14:37:15 -0700

    acp

--- src/main/java/com/revature/daos/PaymentDAOInterface.java
+++ src/main/java/com/revature/daos/PaymentDAOInterface.java
@@ -12,5 +12,5 @@ public interface PaymentDAOInterface {
     Payment insertPayment(Payment pay);
 
     //UPDATE
-//  boolean updatePayment(String bill, String due_date);
+    boolean updatePayment(String bill, String due_date);
 }
